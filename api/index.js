70df713dbe6490132aec255252996ce3c61309b7
abdul8840 <abdul14941naseer@gmail.com>
2024-10-27 11:47:43 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -4,13 +4,18 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
-    console.log('MongoDb is connected');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const PORT = process.env.PORT
+
+mongoose.connect(process.env.MONGO).then(
+  () => console.log('Connected to MongoDB'),
+).catch(
+  (err) => console.error(err),
+)
 
 const app = express();
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log('Server is running on port 8000!');
+});
