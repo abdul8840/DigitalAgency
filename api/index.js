d92ad7dd8ce5df6a501118a35cb780a1769b09b1
abdul8840 <abdul14941naseer@gmail.com>
2024-10-27 12:59:05 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -22,3 +23,4 @@ app.listen(PORT, () => {
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
